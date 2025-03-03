4a7a1501abf1a6a186cd851264781b169de6976a
ScottWhite2004 <SCOTT.WHITE-2021@hull.ac.uk>
2025-03-03 16:35:19 +0000

    Working on matrix multiplication using cuda
    CUDA single threaded matrix multiplication

---
+++ Cuda Lab 5/Cuda Lab 5 Exercise 1.2/kernel.cu
@@ -0,0 +1,71 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+
+__global__ void addKernel(int *c, const int *a, const int *b, const int *widthA,const int *widthB)
+{
+    int i = threadIdx.y;
+    int j = threadIdx.x;
+    int C_ij = i * blockDim.x + j;
+
+    int tempC = 0;
+    for (int k = 0; k < *widthA; k++)
+    {
+        int i_A = i * *widthA + k;
+        int i_B = j + k * *widthB;
+        tempC += a[i_A] * b[i_B];
+    }
+    c[C_ij] = tempC;
+    
+}
+
+int main()
+{
+    
+    const int heightA = 4;
+    const int widthA = 3;
+    const int heightB = 3;
+    const int widthB = 2;
+    const int arraySizeA = heightA * widthA;
+    const int arraySizeB = heightB * widthB;
+    const int arraySizeC = heightA * widthB;
+
+    const int a[arraySizeA] = { 1,2,3,1,2,3,1,2,3,1,2,3 };
+    const int b[arraySizeB] = { 1,2,1,2,1,2 };
+    int c[arraySizeC] = { 0 };
+    
+    int* dev_a, * dev_b, * dev_c;
+    int* dev_widA, *dev_widB;
+
+    cudaMalloc(&dev_a, arraySizeA * sizeof(int));
+    cudaMalloc(&dev_b, arraySizeB * sizeof(int));
+    cudaMalloc(&dev_c, arraySizeC * sizeof(int));
+    cudaMalloc(&dev_widA, sizeof(int));
+    cudaMalloc(&dev_widB, sizeof(int));
+
+    cudaMemcpy(dev_a, a, arraySizeA * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_b, b, arraySizeB * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_widA, &widthA, arraySizeA * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(dev_widB, &widthB, arraySizeB * sizeof(int), cudaMemcpyHostToDevice);
+
+    addKernel << <8,8>> > (dev_a, dev_b, dev_c, dev_widA, dev_widB);
+
+    cudaMemcpy(c, dev_c, arraySizeC * sizeof(int), cudaMemcpyDeviceToHost);
+
+    cudaDeviceSynchronize();
+
+    printf("Dot product %d", c[1]);
+
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    cudaFree(dev_c);
+    cudaFree(dev_widA);
+    cudaFree(dev_widB);
+    
+    return 0;
+}
+
+
--- Cuda Lab 5/Cuda Lab 5/kernel.cu
+++ Cuda Lab 5/Cuda Lab 5/kernel.cu
@@ -7,11 +7,8 @@
 
 int main()
 {
-
-    
     const int heightA = 4;
     const int widthA = 3;
-
     const int heightB = 3;
     const int widthB = 2;
 
@@ -26,16 +23,22 @@ int main()
         {1,2},
         {1,2}
     };
-
-    const int arraySizeA = heightA * heightB;
-    const int arraySizeB = heightB * widthB;
     const int arraySizeC = heightA * widthB;
+    int matrixC[heightA][widthB];
 
+    for (int i = 0; i < heightA; i++)
+    {
+        for (int j = 0; j < widthB; j++)
+        {
+            matrixC[i][j] = 0;
+            for (int idx = 0; idx < widthA; idx++)
+            {
+                matrixC[i][j] += matrixA[i][idx] * matrixB[idx][j];
+            }
+        }
+    }
 
-
-    
-
-
+    printf("Matrix %d %d %d %d %d %d %d %d", matrixC[0][0], matrixC[0][1], matrixC[1][0], matrixC[1][1], matrixC[2][0], matrixC[2][1], matrixC[3][0], matrixC[3][1]);
     return 0;
 }
 
